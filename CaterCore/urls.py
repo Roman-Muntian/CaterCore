cb20eaea0201718edf38c1d6d2c4edabebf38c93
Roman Muntian <muntian.roman.clg@chnu.edu.ua>
2025-10-23 18:48:33 +0200

    add CaterCore

--- CaterCore/urls.py
+++ CaterCore/urls.py
@@ -1,22 +1,29 @@
-"""
-URL configuration for CaterCore project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# config/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    # # Вебхуки та API поза i18n
+    # path('services/', include('services.urls', namespace='services')),
 ]
+
+# Маршрути, що підтримують переклад
+urlpatterns += i18n_patterns(
+    # path('users/', include('users.urls', namespace='users')),
+    # path('cart/', include('cart.urls', namespace='cart')),
+    # path('orders/', include('orders.urls', namespace='orders')),
+    # path('reviews/', include('reviews.urls', namespace='reviews')),
+    # path('', include('catalog.urls', namespace='catalog')),
+    
+    # # Flatpages (має бути останнім)
+    # path('', include('django.contrib.flatpages.urls')),
+)
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
